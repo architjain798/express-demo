53a93628dc9ab3a489f850659cc38e425d854671
Archit Jain <architjain798@gmail.com>
2023-01-28 18:36:57 +0530

    Added the route

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PUBLIC_PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/movies", (req, res) => {
+  let arr = [
+    {
+      name: "abcd",
+      date: new Date(),
+    },
+    {
+      name: "xyz",
+      date: new Date(),
+    },
+  ];
+  res.send({ movies: arr });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
